c67a63288ea969ff66eb144be9f9f0ef36c3ec09
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-09-02 21:27:07 +0100

    sameSite

--- controllers/authController.js
+++ controllers/authController.js
@@ -100,8 +100,8 @@ exports.login = async (req, res) => {
 
         res.cookie('authToken', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Strict',
+secure: process.env.NODE_ENV === 'production' ? true : false,
+    sameSite: 'None',
     maxAge: 60 * 60 * 1000
 });
 
